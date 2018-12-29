db73e8a73bda40d9885d7e1e5e48e94238143eee
Ben Jacobson <ben_jacobson@live.com>
2018-12-30 09:22:19 +1100

    completed layout and styling tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -52,59 +52,64 @@ class FunctionalTest(StaticLiveServerTestCase):
 
 class LayoutAndStylingTest(FunctionalTest):
     def test_layout_and_styling(self):
-        self.fail('finish the test')
+        # user visits the home page
+        self.browser.get(self.live_server_url)
+
+        # user notices that there are rows of game cards, 3 columns to the row
+        game_cards_row = self.browser.find_element_by_class_name('games-list-row')
+        self.assertEqual(3, len(game_cards_row.find_elements_by_class_name('game-listview')))
 
 class HomePageTests(FunctionalTest):
     def test_visit_home_page_and_test_genre_dropdown(self):
         # ensure that the genre dropdown menu works
-        pass
+        self.fail('finish the test')
 
     def test_visit_home_page_and_visit_game_page(self):
         # ensure that you can click on a game from the home page
 
         # check that you can click the link
         # check that you can click on the image
-        pass
+        self.fail('finish the test')
 
     def test_game_card_links_to_genre(self):
         # ensure that the game cards have working link to a genre filter from the home page
-        pass
+        self.fail('finish the test')
 
     def test_game_card_links_to_publisher_filter(self):
         # ensure that the game cards have working link to a publisher filter from the home page
-        pass
+        self.fail('finish the test')
 
     def test_visit_home_page_pagination(self):
         # check that you can click on next page
         # check that you can click on a page number
         # check that you can click on prev page 
-        pass
+        self.fail('finish the test')
 
     def test_visit_home_page_and_visit_publisher_page(self):
         # does the right title appear? 
         # do the right publishers appear?
-        pass
+        self.fail('finish the test')
     
     def test_visit_home_page_and_filter_by_genre(self):
         # does the right title appear
         # do the right games appear? 
-        pass
+        self.fail('finish the test')
 
 class GamePageTests(FunctionalTest):
     def test_visit_game_page_and_test_screenshots(self): 
         # when you visit a game page directly, do the screenshots appear? 
-        pass
+        self.fail('finish the test')
 
     def test_visit_game_page_and_download_game(self): 
         # when you visit a game page directly, do download_links_appear? 
-        pass
+        self.fail('finish the test')
 
     def test_visit_game_page_logo_and_all_games_returns_to_homepage(self):
         # when on a game page, can you click on game logo and go back to home page?
         # when on a game page, can you click on 'all games'
-        pass
+        self.fail('finish the test')
 
 class PublisherPageTests(FunctionalTest):
     def test_visit_publisher_page_and_select_a_publisher_filter(self):
         # visit publisher page, click a publisher, do the right games appear? 
-        pass
+        self.fail('finish the test')
