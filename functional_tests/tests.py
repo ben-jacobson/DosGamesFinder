062c9c23fd15d8846bfe31b8c3951cd15c7ee6ed
Ben Jacobson <ben_jacobson@live.com>
2019-01-01 13:45:19 +1100

    completed some more functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -19,13 +19,20 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.test_publisher_test_soft = create_test_publisher(name='Test Soft Inc')
         self.test_publisher_foobar_games = create_test_publisher(name='Foo Bar Entertainment')
               
-        # create 6 test dosgames.
+        # create 6 specific test dosgames.
         self.test_dosgame_a = create_test_dosgame(title='Abracadabra', publisher=self.test_publisher_test_soft, genre=self.test_action_genre)
         self.test_dosgame_b = create_test_dosgame(title='Beetlejuice', publisher=self.test_publisher_test_soft, genre=self.test_action_genre)
         self.test_dosgame_c = create_test_dosgame(title='Commandant Ki', publisher=self.test_publisher_test_soft, genre=self.test_adventure_genre)
         self.test_dosgame_d = create_test_dosgame(title='Dodecahedron', publisher=self.test_publisher_foobar_games, genre=self.test_adventure_genre)
         self.test_dosgame_e = create_test_dosgame(title='Explorer Dora', publisher=self.test_publisher_foobar_games, genre=self.test_action_genre)
         self.test_dosgame_f = create_test_dosgame(title='Fortune Finder', publisher=self.test_publisher_foobar_games, genre=self.test_action_genre)
+
+        # to test pagination, we'll create a bunch of dummy anonoymous test games too. So as to keep our filtering tests clean, we'll create separate genre and publisher too.
+        test_publisher = create_test_publisher('Throwaway Games')
+        test_genre = create_test_genre('Shovelware')
+
+        for c in 'abcdefghijklmnopqrstuvwxyz1234567890': # Note that there are separate unit tests for testing the code that enables/disables pagination
+            create_test_dosgame(title=c, genre=test_genre, publisher=test_publisher)
 
         # create a test screenshot for each game
         self.test_screenshot_a = create_test_screenshot(game=self.test_dosgame_a)
@@ -97,19 +104,48 @@ class HomePageTests(FunctionalTest):
         self.assertIn('/game/', self.browser.current_url)
 
     def test_game_card_links_to_genre(self):
-        # ensure that the game cards have working link to a genre filter from the home page
-        self.fail('finish the test')
+        # user visits the home page and sees a few games on the home page
+        self.browser.get(self.live_server_url)
+
+        # user sees that the game card has a genre link and clicks it
+        listview_game_genre = self.browser.find_element_by_class_name('card-genre').find_element_by_tag_name('a')
+        listview_game_genre_name = listview_game_genre.text
+        listview_game_genre.click()
+
+        # user is redirected to a page filtering by that genre. User notices that the title now says something like 'action games' or 'adventure games'
+        filterview_page_title =  self.browser.find_element_by_tag_name('h1')
+        self.assertEqual(str(listview_game_genre_name) + ' Games', filterview_page_title.text)
 
     def test_game_card_links_to_publisher_filter(self):
-        # ensure that the game cards have working link to a publisher filter from the home page
-        self.fail('finish the test')
+        # user visits the home page and sees a few games on the home page
+        self.browser.get(self.live_server_url)
+
+        # user sees that the game card has a genre link and clicks it
+        listview_game_publisher = self.browser.find_element_by_class_name('card-publisher-and-date').find_element_by_tag_name('a')
+        listview_game_publisher_name = listview_game_publisher.text
+        listview_game_publisher.click()
+
+        # user is redirected to a page filtering by that genre. User notices that the title now says something like 'action games' or 'adventure games'
+        filterview_page_title =  self.browser.find_element_by_tag_name('h1')
+        self.assertEqual('Games by ' + str(listview_game_publisher_name), filterview_page_title.text)
 
     def test_visit_home_page_pagination(self):
-        # check that you can click on next page
-        # check that you can click on a page number
-        # check that you can click on prev page 
-        self.fail('finish the test')
+        # user visits the home page and sees a few games on the home page
+        self.browser.get(self.live_server_url)        
+        
+        # user notices that there are some page numbers across the top of the page. 
+        # user clicks the number 2 and notices that the url changes
+        self.browser.find_element_by_link_text('2').click()
+        self.assertIn('/?page=2', self.browser.current_url)
 
+        # user then clicks the prev page button and is taken back to page 1
+        self.browser.find_element_by_link_text('Prev').click()
+        self.assertIn('/?page=1', self.browser.current_url)
+
+        # user then clicks the next page button and is taken back to page 2    
+        self.browser.find_element_by_link_text('Next').click()
+        self.assertIn('/?page=2', self.browser.current_url)
+        
     def test_visit_home_page_and_visit_publisher_page(self):
         # does the right title appear? 
         # do the right publishers appear?
